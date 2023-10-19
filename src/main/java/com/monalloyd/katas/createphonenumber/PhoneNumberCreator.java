c890cc358c7a8905c32153edf462d874bd2fc17b
monalloyd <monapluslloyd@gmail.com>
2023-10-19 08:51:16 +0200

    Update phone number creator with more concise method

--- src/main/java/com/monalloyd/katas/createphonenumber/PhoneNumberCreator.java
+++ src/main/java/com/monalloyd/katas/createphonenumber/PhoneNumberCreator.java
@@ -3,6 +3,7 @@ package com.monalloyd.katas.createphonenumber;
 import java.util.stream.IntStream;
 
 public class PhoneNumberCreator {
+
     public String create(int[] digits) {
         return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(digits).boxed().toArray());
     }
